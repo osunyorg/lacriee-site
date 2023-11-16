8bfdb55001ceb003913b08a13f50bd15e2e10b2d
Olivia206 <olivia.simonet@mmibordeaux.com>
2023-11-16 17:04:44 +0100

    added cursor for image and gallery block

--- assets/js/design-system/cursor.js
+++ assets/js/design-system/cursor.js
@@ -11,15 +11,29 @@ class Cursor {
     }
 
     listen () {
-        const a = document.querySelectorAll('.block a, .events__section .events a');
+        const a = document.querySelectorAll('.block:not(.block-gallery) a, .events__section .events a');
+        const images = document.querySelectorAll('.block.block-gallery a, .block.block-image a');
+
         window.addEventListener('mousemove', (e) => {
             this.element.style.left = e.clientX + 'px'
             this.element.style.top = e.clientY + 'px'
         });
+
         a.forEach((element) => {
             element.style.cursor = 'none';
             element.addEventListener('mouseenter', (e) => {
                 this.show()
+            });
+            element.addEventListener('mouseleave', (e) => {
+                this.hide()
+            });
+        });
+        
+        images.forEach((element) => {
+            element.style.cursor = 'none';
+            element.addEventListener('mouseenter', (e) => {
+                this.show();
+                this.element.classList.add('extend')
             });
             element.addEventListener('mouseleave', (e) => {
                 this.hide()
